3788bcf99a6a3becc4f87aae9780b3f5b4aeff8b
HugoEsparzaC <hugo.esparza3@gmail.com>
2023-01-30 23:27:10 -0600

    Nuevo Diseño

--- Customer_Billing_Cine.py
+++ Customer_Billing_Cine.py
@@ -8,10 +8,14 @@ class Cliente(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Sistema de Facturación de clientes")
-        self.geometry("1350x750")
+        self.geometry("1366x768+0+0")
         self.iconbitmap('Cine.ico')
         self.config(background="white")
         self.hora = tk.StringVar()
+        self.num_boletos = tk.StringVar()
+        self.num_palomitas = tk.StringVar()
+        self.num_refrescos = tk.StringVar()
+        self.num_acompanamientos = tk.StringVar()
         self.nombre = tk.StringVar()
         self.pelicula = tk.StringVar()
         self.sala = tk.StringVar()
@@ -56,61 +60,85 @@ class Cliente(tk.Tk):
         self.lbl_titulo=tk.Label(ABC1, text='            Sistema de Facturación de Clientes         ', font=('arial', 30, 'bold'), pady=9, bd=5, bg='royal blue', fg="white", justify=tk.CENTER).grid(row=0, column=1)
         self.lbl_hora=tk.Label(ABC1, textvariable=self.hora, font=('arial', 30, 'bold'), pady=9, bd=5, bg='royal blue', fg="white").grid(row=0, column=2)
         # =============================================================================================
-        # Cliente, Pelicula y Sala
+        # Numero de boletos, Pelicula y Sala
         # =============================================================================================
-        self.lbl_cliente = tk.Label(ABC2, font=('arial', 12, 'bold'), text="Nombre Cliente:", padx=2, fg="black", bg='white').grid(row=0, column=0, sticky='w')
-        self.txt_cliente = tk.Entry(ABC2, font=('arial', 12, 'bold'), textvariable= self.nombre, width=30, relief='solid')
-        self.txt_cliente.grid(row=0, column=1, pady=3)
+        self.lbl_Pelicula = tk.Label(ABC2, font=('arial', 16, 'bold'), text="Peliculas", padx=2, fg="black", bg='white').grid(row=0, column=0, sticky='w')
+        self.lbl_num_boletos = tk.Label(ABC2, font=('arial', 12, 'bold'), text="Número de boletos:", padx=2, fg="black", bg='white').grid(row=1, column=0, sticky='w')
+        self.txt_num_boletos = tk.Entry(ABC2, font=('arial', 12, 'bold'), textvariable= self.num_boletos, width=31, relief='solid')
+        self.txt_num_boletos.grid(row=1, column=1, pady=3)
 
-        self.lbl_pelicula = tk.Label(ABC2, font=('arial', 12, 'bold'), text="Pelicula:", padx=2, fg="black", bg='white').grid(row=1, column=0, sticky='w')
-        self.caja_pelicula = ttk.Combobox(ABC2, textvariable=self.pelicula, state='readonly', font=('arial', 12, 'bold'), width=28)
+        self.lbl_pelicula = tk.Label(ABC2, font=('arial', 12, 'bold'), text="Pelicula:", padx=2, fg="black", bg='white').grid(row=2, column=0, sticky='w')
+        self.caja_pelicula = ttk.Combobox(ABC2, textvariable=self.pelicula, state='readonly', font=('arial', 12, 'bold'), width=29)
         self.caja_pelicula['value'] = ('', 'AVENGERS', 'LA MONJA', 'EL HOMBRE ARAÑA', 'DRAGON BALL')
         self.caja_pelicula.current(0)
-        self.caja_pelicula.grid(row=1, column=1, pady=3)
+        self.caja_pelicula.grid(row=2, column=1, pady=3)
 
-        self.lbl_sala = tk.Label(ABC2, font=('arial', 12, 'bold'), text="Sala:", padx=2, fg="black", bg='white').grid(row=2, column=0, sticky='w')
-        self.caja_sala = ttk.Combobox(ABC2, textvariable=self.sala, state='readonly', font=('arial', 12, 'bold'), width=28)
+        self.lbl_sala = tk.Label(ABC2, font=('arial', 12, 'bold'), text="Sala:", padx=2, fg="black", bg='white').grid(row=3, column=0, sticky='w')
+        self.caja_sala = ttk.Combobox(ABC2, textvariable=self.sala, state='readonly', font=('arial', 12, 'bold'), width=29)
         self.caja_sala['value'] = ('', 'SENCILLA', '3D')
         self.caja_sala.current(0)
-        self.caja_sala.grid(row=2, column=1, pady=3)
+        self.caja_sala.grid(row=3, column=1, pady=3)
+
+        self.boton_Agregar_Pelicula = tk.Button(ABC2, padx=0, pady=0, bd=5, fg='black', font=('arial', 12, 'bold'), width=1, height=1, bg='sky blue', text="Agregar Pedido", command=self._Total).grid(row=4, column=0, columnspan=2, sticky='we')
         # =============================================================================================
-        # Palomitas, Refresco y acompanamiento
+        # Palomitas, Refresco y acompanamiento (agregar cantidades de cada uno)
         # =============================================================================================
-        self.lbl_palomitas = tk.Checkbutton(ABC3, text='Palomitas', variable=self.b_palomitas, onvalue=1, offvalue=0, font=('arial', 12, 'bold'), bg='white', command=self._Palomitas)
-        self.lbl_palomitas.grid(row=0, sticky='w')
-        self.caja_palomitas = ttk.Combobox(ABC3, textvariable=self.palomitas, state='disabled', font=('arial', 12, 'bold'), width=30)
+        self.lbl_Comida = tk.Label(ABC2, font=('arial', 16, 'bold'), text="Comida", padx=2, fg="black", bg='white').grid(row=5, column=0, sticky='w')
+        self.lbl_palomitas = tk.Checkbutton(ABC2, text='Palomitas', variable=self.b_palomitas, onvalue=1, offvalue=0, font=('arial', 12, 'bold'), bg='white', command=self._Palomitas)
+        self.lbl_palomitas.grid(row=6, sticky='w')
+        self.lbl_num_palomitas = tk.Label(ABC2, font=('arial', 12, 'bold'), text="Número de palomitas:", padx=2, fg="black", bg='white').grid(row=1, column=0, sticky='w')
+        self.txt_num_palomitas = tk.Entry(ABC2, font=('arial', 12, 'bold'), textvariable= self.num_palomitas, width=31, relief='solid')
+        self.txt_num_palomitas.grid(row=6, column=1, pady=3)
+        self.caja_palomitas = ttk.Combobox(ABC2, textvariable=self.palomitas, state='disabled', font=('arial', 12, 'bold'), width=29)
         self.caja_palomitas['value'] = ('', 'MANTEQUILLA', 'NATURALES', 'QUESO', 'ACARAMELADAS', 'VALENTINA', 'BUFALO')
         self.caja_palomitas.current(0)
-        self.caja_palomitas.grid(row=0, column=1, pady=3)
-        self.caja_tam_palomitas = ttk.Combobox(ABC3, textvariable=self.tam_palomitas, state='disabled', font=('arial', 12, 'bold'), width=30)
+        self.caja_palomitas.grid(row=7, column=1, pady=3)
+        self.caja_tam_palomitas = ttk.Combobox(ABC2, textvariable=self.tam_palomitas, state='disabled', font=('arial', 12, 'bold'), width=29)
         self.caja_tam_palomitas['value'] = ('', 'CHICA', 'MEDIANA', 'GRANDE', 'PARA LLEVAR')
         self.caja_tam_palomitas.current(0)
-        self.caja_tam_palomitas.grid(row=2, column=1, pady=3)
+        self.caja_tam_palomitas.grid(row=8, column=1, pady=3)
 
-        self.lbl_refresco = tk.Checkbutton(ABC3, text='Refresco', variable=self.b_refresco, onvalue=1, offvalue=0, font=('arial', 12, 'bold'), bg='white', command=self._Refresco).grid(row=3, sticky='w')
-        self.caja_refresco = ttk.Combobox(ABC3, textvariable=self.refresco, state='disabled', font=('arial', 12, 'bold'), width=30)
-        self.caja_refresco['value'] = ('', 'COCA COLA', 'COCA COLA S/A', 'SPRITE', 'MANZANA', 'FANTA', 'FUZE TEA')
-        self.caja_refresco.current(0)
-        self.caja_refresco.grid(row=3, column=1, pady=3)
-        self.caja_tam_refresco = ttk.Combobox(ABC3, textvariable=self.tam_refresco, state='disabled', font=('arial', 12, 'bold'), width=30)
-        self.caja_tam_refresco['value'] = ('', 'CHICO', 'MEDIANO', 'GRANDE', 'JUMBO')
-        self.caja_tam_refresco.current(0)
-        self.caja_tam_refresco.grid(row=4, column=1, pady=3)
-
-        self.lbl_acompanamiento = tk.Checkbutton(ABC3, text='Acompañamiento', variable=self.b_acompanamiento, onvalue=1, offvalue=0, font=('arial', 12, 'bold'), bg='white', command=self._Acompanamiento).grid(row=5, sticky='w')
-        self.caja_acompanamiento = ttk.Combobox(ABC3, textvariable=self.acompanamiento, state='disabled', font=('arial', 12, 'bold'), width=30)
+        self.lbl_acompanamiento = tk.Checkbutton(ABC2, text='Acompañamiento', variable=self.b_acompanamiento, onvalue=1, offvalue=0, font=('arial', 12, 'bold'), bg='white', command=self._Acompanamiento).grid(row=9, sticky='w')
+        self.txt_num_acompanamiento = tk.Entry(ABC2, font=('arial', 12, 'bold'), textvariable= self.num_acompanamientos, width=31, relief='solid')
+        self.txt_num_acompanamiento.grid(row=9, column=1, pady=3)
+        
+        self.caja_acompanamiento = ttk.Combobox(ABC2, textvariable=self.acompanamiento, state='disabled', font=('arial', 12, 'bold'), width=29)
         self.caja_acompanamiento['value'] = ('', 'NACHOS', 'HOT-DOG', 'HOT-DOG JUMBO', 'CHOCOLATE', 'HELADO', 'DULCES')
         self.caja_acompanamiento.current(0)
-        self.caja_acompanamiento.grid(row=5, column=1, pady=3)
-        self.lbl_queso = tk.Checkbutton(ABC3, text='Queso extra', variable=self.b_queso, onvalue=1, offvalue=0, font=('arial', 12, 'bold'), bg='white', state='disabled', command=self._Queso)
-        self.lbl_queso.grid(row=6, column=1, sticky='w')
+        self.caja_acompanamiento.grid(row=10, column=1, pady=3)
+        self.lbl_queso = tk.Checkbutton(ABC2, text='Queso extra', variable=self.b_queso, onvalue=1, offvalue=0, font=('arial', 12, 'bold'), bg='white', state='disabled', command=self._Queso)
+        self.lbl_queso.grid(row=11, column=1, sticky='w')
+
+        self.boton_Agregar_Comida = tk.Button(ABC2, padx=0, pady=0, bd=5, fg='black', font=('arial', 12, 'bold'), width=1, height=1, bg='sky blue', text="Agregar Pedido", command=self._Total).grid(row=12, column=0, columnspan=2, sticky='we')
+
+
+
+
+
+        self.lbl_refresco = tk.Checkbutton(ABC2, text='Refresco', variable=self.b_refresco, onvalue=1, offvalue=0, font=('arial', 12, 'bold'), bg='white', command=self._Refresco).grid(row=13, sticky='w')
+        self.txt_num_refrescos = tk.Entry(ABC2, font=('arial', 12, 'bold'), textvariable= self.num_refrescos, width=31, relief='solid')
+        self.txt_num_refrescos.grid(row=13, column=1, pady=3)
+
+        self.caja_refresco = ttk.Combobox(ABC2, textvariable=self.refresco, state='disabled', font=('arial', 12, 'bold'), width=29)
+        self.caja_refresco['value'] = ('', 'COCA COLA', 'COCA COLA S/A', 'SPRITE', 'MANZANA', 'FANTA', 'FUZE TEA')
+        self.caja_refresco.current(0)
+        self.caja_refresco.grid(row=14, column=1, pady=3)
+        self.caja_tam_refresco = ttk.Combobox(ABC2, textvariable=self.tam_refresco, state='disabled', font=('arial', 12, 'bold'), width=29)
+        self.caja_tam_refresco['value'] = ('', 'CHICO', 'MEDIANO', 'GRANDE', 'JUMBO')
+        self.caja_tam_refresco.current(0)
+        self.caja_tam_refresco.grid(row=15, column=1, pady=3)
+        self.boton_Agregar_Refresco = tk.Button(ABC2, padx=0, pady=0, bd=5, fg='black', font=('arial', 12, 'bold'), width=1, height=1, bg='sky blue', text="Agregar Pedido", command=self._Total).grid(row=16, column=0, columnspan=2, sticky='we')
+        # =============================================================================================
+        # Combos
+        # =============================================================================================
+
         # =============================================================================================
         # Ticket
         # =============================================================================================
         self.ticket = tk.Text(ABC5, height=25, width=43, bd=10, state = 'disabled', font=('arial', 9, 'bold'), wrap=tk.WORD)
         self.ticket.grid(row=0, column=0)
         # =============================================================================================
-        # Botones Total, limpiar, salir
+        # Botones Total, limpiar, agregar pedido
         # =============================================================================================
         self.boton_Total = tk.Button(ABC6, padx=14, pady=7, bd=5, fg='black', font=('arial', 16, 'bold'), width=5, height=2, bg='white', text="Total", command=self._Total).grid(row=0, column=0)
         self.boton_limpiar = tk.Button(ABC6, padx=14, pady=7, bd=5, fg='black', font=('arial', 16, 'bold'), width=5, height=2, bg='white', text="Limpiar", command=self._Limpiar).grid(row=0, column=1)
